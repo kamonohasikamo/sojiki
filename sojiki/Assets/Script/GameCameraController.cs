14dac894c7598e8c03790c5813e472d1a8cc52c7
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-07-03 22:16:27 +0900

    壁の配置＆壁と床のマテリアル追加＆キャラの移動＆カメラの回転追加

---
+++ sojiki/Assets/Script/GameCameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCameraController : MonoBehaviour {
+	public GameObject targetObj;
+	public Vector3 targetPos;
+
+	void Start() {
+		targetPos = targetObj.transform.position;
+	}
+
+	void Update() {
+		// targetの移動量分、自分（カメラ）も移動する
+		transform.position += targetObj.transform.position - targetPos;
+		targetPos = targetObj.transform.position;
+ 
+		if (Input.GetMouseButton(1)) {
+			float mouseInputX = Input.GetAxis("Mouse X");
+			float mouseInputY = Input.GetAxis("Mouse Y");
+			
+			// targetの位置のY軸を中心に、回転（公転）する
+			transform.RotateAround(targetPos, Vector3.up, mouseInputX * Time.deltaTime * 200f);
+			// カメラの垂直移動（※角度制限なし、必要が無ければコメントアウト）
+			//transform.RotateAround(targetPos, transform.right, mouseInputY * Time.deltaTime * 200f);
+		}
+	}
+}
---
+++ sojiki/Assets/Script/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public GameObject playerHand;
+	public GameObject gameCamera;
+	void Start() {
+		//playerHand.transform.rotation = Quaternion.Euler(0, 0, 0);
+	}
+
+	void Update() {
+		Vector3 cameraPosition = Input.mousePosition;
+		cameraPosition.z = 10.0f;
+		playerHand.transform.position = Camera.main.ScreenToWorldPoint(cameraPosition); 
+		//playerHand.transform.rotation = gameCamera.transform.rotation;
+	}
+}
