a8d185f9fe610ec3e25c7d228421542e0aafe262
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-07-05 21:18:29 +0900

    制限時間とスコアのUIを作ったよ

---
+++ sojiki/Assets/Script/ScoreController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+	public Text ScoreText;
+
+	void Start() {
+		ScoreText.text = "0点"; 
+	}
+
+	void Update() {
+		
+	}
+}
---
+++ sojiki/Assets/Script/TimerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TimerController : MonoBehaviour {
+
+	public Text timerText;
+ 
+	public float totalTime;
+	public static int seconds;
+	void Start() {
+		timerText.text = "残り時間：" + seconds.ToString() + "秒";
+	}
+
+	void Update() {
+		totalTime -= Time.deltaTime;
+		seconds = (int)totalTime;
+		if (seconds <= 0) {
+			seconds = 0;
+		}
+		timerText.text = "残り時間：" + seconds.ToString() + "秒";
+	}
+}
