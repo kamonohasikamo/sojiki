ee77205ea194391a287d865434e4ec53fe1e0f83
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-07-07 02:18:17 +0900

    説明文の追加

---
+++ sojiki/Assets/Script/GameStart/ShowTextController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowTextController : MonoBehaviour {
+	public GameObject objText;
+	public Text UIText;
+	void Start() {
+		UIText = objText.GetComponent<Text>();
+		string filePath = Application.dataPath + @"\Resources\GameDescription.txt";
+ 
+		//　ファイルが存在しなければ作成
+		if (!File.Exists (filePath)) {
+			using (File.Create (filePath)) {
+			}
+		}
+ 
+		string allText2 = File.ReadAllText (filePath);
+		UIText.text = allText2;
+	}
+
+}
---
+++ sojiki/Assets/Script/GameStart/TitleBackScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class TitleBackScript : MonoBehaviour {
+	public void OnClickBackFunction() {
+		SceneManager.LoadScene("Start");
+	} 
+}
