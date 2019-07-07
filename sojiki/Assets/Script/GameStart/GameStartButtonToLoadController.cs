1c82ae3d7dcb2e342656df5d760148524a0ce190
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-07-07 13:31:05 +0900

    説明文の文言の修正とロード画面の追加

---
+++ sojiki/Assets/Script/GameStart/GameStartButtonToLoadController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameStartButtonToLoadController : MonoBehaviour {
+
+	//　非同期動作で使用するAsyncOperation
+	private AsyncOperation async;
+	//　シーンロード中に表示するUI画面
+	[SerializeField]
+	private GameObject loadUI;
+	//　読み込み率を表示するスライダー
+	[SerializeField]
+	private Slider slider;
+	public void OnClickStartFunction() {
+		//　ロード画面UIをアクティブにする
+		loadUI.SetActive(true);
+ 
+		//　コルーチンを開始
+		StartCoroutine("LoadStart");
+	}
+
+	IEnumerator LoadStart() {
+		async = SceneManager.LoadSceneAsync("GameScene");
+
+		while(!async.isDone) {
+			var progressVal = Mathf.Clamp01(async.progress / 0.9f);
+			slider.value = progressVal;
+			yield return null;
+		}
+	}
+}
